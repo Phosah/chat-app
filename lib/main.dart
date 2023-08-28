29eaaf15352b3d0ab6ce0f3427f809a4fa9d1dd8
Phosah <efosa.uyi@gmail.com>
2023-08-28 12:44:59 +0100

    Create Stats page and folder layout

--- lib/main.dart
+++ lib/main.dart
@@ -22,25 +22,26 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'FlutterChat',
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
-        ),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (ctx, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const SplashScreen();
-            }
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
 
-            if (snapshot.hasData) {
-              return const ChatScreen();
-            }
+          if (snapshot.hasData) {
+            return const ChatScreen();
+          }
 
-            return const AuthScreen();
-          },
-        ));
+          return const AuthScreen();
+        },
+      ),
+    );
   }
 }
